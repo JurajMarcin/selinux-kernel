8212583a2ade39a744f86a423783173c7ea8fbbd
Juraj Marcin <juraj@jurajmarcin.com>
2023-11-07 15:49:25 +0100

    fix rule precedence

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -1694,6 +1694,15 @@ static int filename_compute_type(struct policydb *policydb,
 	ft.tclass = tclass;
 	ft.name = objname;
 
+	/* Search for exact rules */
+	ft.name = objname;
+	datum = policydb_filenametr_search(policydb, FILENAME_TRANS_MATCH_EXACT,
+					   &ft, stype);
+	if (datum) {
+		newcontext->type = datum->otype;
+		return 0;
+	}
+
 	/* Search for prefix rules */
 	name_copy = kstrdup(objname, GFP_KERNEL);
 	if (!name_copy)
@@ -1723,13 +1732,6 @@ static int filename_compute_type(struct policydb *policydb,
 			return 0;
 		}
 	}
-
-	/* Search for exact rules */
-	ft.name = objname;
-	datum = policydb_filenametr_search(policydb, FILENAME_TRANS_MATCH_EXACT,
-					   &ft, stype);
-	if (datum)
-		newcontext->type = datum->otype;
 	return 0;
 }
 
